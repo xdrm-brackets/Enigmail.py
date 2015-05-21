ea70ee296f6cb13be39ae42c4d00b64d24838350
xdrm <doowap31@gmail.com>
2015-05-21 21:47:51 +0200

    Ajout majuscules

--- source.py
+++ source.py
@@ -67,7 +67,7 @@ def codeChar(pChar, pSIGMA, pROTOR):
 #############################################################################################################################
 
 # ALPHABET 
-SIGMA = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' '];
+SIGMA = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' '];
 # NOMBRE DE ROTORS
 LEVEL = 3; # valeur par defaut
 LEVEL = int( raw_input('Level: ') );
