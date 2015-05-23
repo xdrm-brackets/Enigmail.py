54573b1b5b7dcc1cce7316082d06b41d5ca00ad5
xdrm-brackets <doowap31@gmail.com>
2015-05-23 18:30:21 +0200

    Update shell && #fix shell alias [stable~65]

--- enigmail/enigmail.sh
+++ enigmail/enigmail.sh
@@ -3,6 +3,7 @@
 if [ $# -eq 1 ]
 then # si 1 paramètre
 	case $1 in
+		'init') echo """\nalias enigmail="sh $(pwd)/enigmail.sh"\n""";;
 		'config') nano .config ;;
 		'write')  nano bucket-file ;; 
 		'read')  echo "\n======================================="; cat bucket-file; echo "\n=======================================";; 
