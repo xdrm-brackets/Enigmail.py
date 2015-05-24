eb345286870b74ac67c5c6d452d379db32b8ce36
xdrm-brackets <doowap31@gmail.com>
2015-05-24 15:25:23 +0200

    SHELL[init config help read write empty encode decode send] + création du fichier help [stable~85]

--- enigmail/enigmail.sh
+++ enigmail/enigmail.sh
@@ -5,6 +5,7 @@ path=$(readlink -f $(dirname $0))
 if [ $# -eq 1 ]
 then # si 1 paramètre
 	case $1 in
+		'help') cat "$path/source/help"|less;;
 		'init')                               # initialise le contenu du fichier de config
 			echo "smtp_server = smtp.gmail.com" > "$path/.config"
 			echo "smtp_port = 587" >> "$path/.config"
@@ -12,7 +13,7 @@ then # si 1 paramètre
 			;;
 		'config') nano "$path/.config";;      # ouvre en modification le fichier de config
 		'write')  nano "$path/bucket-file";;  # ouvre en modification le bucket file
-		'del')  echo "">"$path/bucket-file";;  # vide le bucket file
+		'empty')  echo "">"$path/bucket-file";;  # vide le bucket file
 			                                  # ouvre en lecture le bucket file
 		'read')  echo "\n======================================="; cat "$path/bucket-file"; echo "\n=======================================";; 
 		'encode') python "$path/source/encode.py";;
